6bc356df61ab950d392e3605ccc6c7b88972518b
italijancic <italijancic@outlook.com>
2023-04-29 12:39:51 -0300

    Initial code

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+
+import usersRouter from './routes/users.router.js'
+import petsRouter from './routes/pets.router.js'
+import adoptionsRouter from './routes/adoption.router.js'
+import sessionsRouter from './routes/sessions.router.js'
+
+const app = express()
+const PORT = process.env.PORT||8080
+
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log('Connected to MONGODB')
+})
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/users',usersRouter)
+app.use('/api/pets',petsRouter)
+app.use('/api/adoptions',adoptionsRouter)
+app.use('/api/sessions',sessionsRouter)
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
